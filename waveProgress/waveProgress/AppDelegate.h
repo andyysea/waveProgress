dc27ae4e17918d1925a8805cd3f51f0de75e900e
YangYingHai <601503988@qq.com>
2016-11-27 16:12:50 +0800

    初始化程序

---
+++ waveProgress/waveProgress/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  waveProgress
+//
+//  Created by 杨应海 on 2016/11/27.
+//  Copyright © 2016年 YYH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
